6ab2053e4c8bd6f92e43a56a9b2cc2f0c5e13079
Garrett112 <gww2075@email.vccs.edu>
2024-02-28 16:10:56 -0500

    fix 1

--- Split-Even-Odd.cpp
+++ Split-Even-Odd.cpp
@@ -16,56 +16,47 @@ public:
         nodeType<int>* oddsStart = nullptr;
         nodeType<int>* evensEnd = nullptr;
         nodeType<int>* oddsEnd = nullptr;
-        evensList.first = evensStart;
-        oddsList.first = oddsStart;
 
-
-        while (current != nullptr) {
-            if (current->info % 2 == 0) {
-                if (evensStart == nullptr) {
-                    evensStart = current;
-                    evensEnd = current;
+        while (current != nullptr) {                //while current pointer is not at the end of the linked list
+            if (current->info % 2 == 0) {           //if the current node is evenly divisible by 2
+                if (evensStart == nullptr) {        //if evensStart equals nullptr (if evens linked list is empty)
+                    evensStart = current;           //set pointer evensStart equal to current
+                    evensEnd = current;             //set pointer evensEnd equal to current
                 }
-                else {
-                    evensEnd->link = current;
-                    evensEnd = evensEnd->link;
+                else {                              //if evensStart does not equal nullptr (if evens linked list is not empty)
+                    evensEnd->link = current;       //set the link of evensEnd to current
+                    evensEnd = evensEnd->link;      //set evensEnd equal to the link of itself
                 }
             }
-            else {
-                if (oddsStart == nullptr) {
-                    oddsStart = current;
-                    oddsEnd = current;
-                }
-                else {
-                    oddsEnd->link = current;
-                    oddsEnd = oddsEnd->link;
+            else {                                  //if the current node is not evenly divisible by 2
+                if (oddsStart == nullptr) {         //if oddsStart equals nullptr (if odds linked list is empty)
+                    oddsStart = current;            //set pointer oddsStart equal to current
+                    oddsEnd = current;              //set pointer oddsEnd equal to current
+                }   
+                else {                              //if oddsStart does not equal nullptr (if odds linked list is not empty) 
+                    oddsEnd->link = current;        //sets the link of oddsEnd to current
+                    oddsEnd = oddsEnd->link;        //sets oddsEnd equal to the link of itself
                 }
             }
-            current = current->link;
+            current = current->link;                //sets current equal to the link of itself (moves current to the next node)
         }
-        if (oddsEnd != nullptr) {
-            oddsEnd->link = nullptr;
+        if (evensStart != nullptr) {                //if evensStart does not equal nullptr (if evens linked list is not empty)
+            evensEnd->link = nullptr;               //sets the link of evensEnd to nullptr (ends the evens linked list)
         }
-        if (evensStart != nullptr) {
-            evensEnd->link = nullptr;
+        if (oddsStart != nullptr) {                 //if oddsStart does not equal nullptr (if odds linked list is not empty) 
+            oddsEnd->link = nullptr;                //sets the link of oddsEnd to nullptr (ends the odds linked list)
         }
-        while (evensStart != nullptr) {
-            cout << evensStart->info << endl;
-            evensStart = evensStart->link;
-        }
-        cout << endl << endl;
-        while (oddsStart != nullptr) {
-            cout << oddsStart->info << endl;
-            oddsStart = oddsStart->link;
-        }
-
+        evensList.first = evensStart;               //sets the head of evensList to evensStart
+        oddsList.first = oddsStart;                 //sets the head of oddsList to oddsStart
     }
+
     void printList() {
         nodeType<int>* current = this->first;
         while (current != nullptr) {
             cout << current->info << endl;
             current = current->link;
         }
+        cout << endl;
     }
 };
 
