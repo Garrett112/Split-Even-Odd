7af8971e023b1f52d2af65233ce79d9d4f75790e
Garrett112 <gww2075@email.vccs.edu>
2024-04-03 12:34:08 -0400

    fix

--- Split-Even-Odd.cpp
+++ Split-Even-Odd.cpp
@@ -12,15 +12,15 @@ public:
         }
 
         nodeType<int>* current = this->first;
-        nodeType<int>* evensStart = nullptr;
-        nodeType<int>* oddsStart = nullptr;
+        evensList.first = nullptr;
+        oddsList.first = nullptr;
         nodeType<int>* evensEnd = nullptr;
         nodeType<int>* oddsEnd = nullptr;
 
         while (current != nullptr) {                //while current pointer is not at the end of the linked list
             if (current->info % 2 == 0) {           //if the current node is evenly divisible by 2
-                if (evensStart == nullptr) {        //if evensStart equals nullptr (if evens linked list is empty)
-                    evensStart = current;           //set pointer evensStart equal to current
+                if (evensList.first == nullptr) {   //if evensStart equals nullptr (if evens linked list is empty)
+                    evensList.first = current;      //set pointer evensStart equal to current
                     evensEnd = current;             //set pointer evensEnd equal to current
                 }
                 else {                              //if evensStart does not equal nullptr (if evens linked list is not empty)
@@ -28,9 +28,9 @@ public:
                     evensEnd = evensEnd->link;      //set evensEnd equal to the link of itself
                 }
             }
-            else {                                  //if the current node is not evenly divisible by 2
-                if (oddsStart == nullptr) {         //if oddsStart equals nullptr (if odds linked list is empty)
-                    oddsStart = current;            //set pointer oddsStart equal to current
+            else if (current->info % 2 != 0) {      //if the current node is not evenly divisible by 2
+                if (oddsList.first == nullptr) {    //if oddsStart equals nullptr (if odds linked list is empty)
+                    oddsList.first = current;       //set pointer oddsStart equal to current
                     oddsEnd = current;              //set pointer oddsEnd equal to current
                 }   
                 else {                              //if oddsStart does not equal nullptr (if odds linked list is not empty) 
@@ -40,23 +40,15 @@ public:
             }
             current = current->link;                //sets current equal to the link of itself (moves current to the next node)
         }
-        if (evensStart != nullptr) {                //if evensStart does not equal nullptr (if evens linked list is not empty)
+        if (evensList.first != nullptr) {           //if evensStart does not equal nullptr (if evens linked list is not empty)
             evensEnd->link = nullptr;               //sets the link of evensEnd to nullptr (ends the evens linked list)
         }
-        if (oddsStart != nullptr) {                 //if oddsStart does not equal nullptr (if odds linked list is not empty) 
+        if (oddsList.first != nullptr) {            //if oddsStart does not equal nullptr (if odds linked list is not empty) 
             oddsEnd->link = nullptr;                //sets the link of oddsEnd to nullptr (ends the odds linked list)
         }
-        evensList.first = evensStart;               //sets the head of evensList to evensStart
-        oddsList.first = oddsStart;                 //sets the head of oddsList to oddsStart
-    }
-
-    void printList() {
-        nodeType<int>* current = this->first;
-        while (current != nullptr) {
-            cout << current->info << endl;
-            current = current->link;
-        }
-        cout << endl;
+        first = nullptr;
+        last = nullptr;
+        count = 0;
     }
 };
 
@@ -81,13 +73,23 @@ int main()
     unorderedList.insertFirst(21);
     unorderedList.insertFirst(62);
     unorderedList.insertFirst(34);
+    cout << "List: ";
+    unorderedList.print();
+    cout << endl;
     intLinkedList evensList;
     intLinkedList oddsList;
     unorderedList.splitEvensOddsList(evensList, oddsList);
-    evensList.printList();
-    oddsList.printList();
-    
-
-
-
+    linkedListIterator<int> printer = evensList.begin();
+    cout << "Evens List: ";
+    while (printer != evensList.end()) {
+        cout << *printer << " ";
+        ++printer;
+    }
+    cout << endl;
+    printer = oddsList.begin();
+    cout << "Odds List: ";
+    while (printer != oddsList.end()) {
+        cout << *printer << " ";
+        ++printer;
+    }
 }
